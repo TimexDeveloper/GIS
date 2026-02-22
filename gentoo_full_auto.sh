2c70f85d0bfa5c67c61a1338cc2b6181fd3ca22a
Timex <timexdev123@gmail.com>
2026-02-22 13:43:40 +0700

    Im done with this script rewrite

--- gentoo_full_auto.sh
+++ gentoo_full_auto.sh
@@ -4,29 +4,35 @@ set -e
 DISK="/dev/sda"
 PART_SWAP="${DISK}1"
 PART_ROOT="${DISK}2"
-# Your specific Stage3 link
+# Stage3 link (the one you wanted)
 STAGE3_URL="https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-desktop-openrc-20260215T164556Z.tar.xz"
+# Snapshot link to fix the "No such file" repo error
+SNAP_URL="https://distfiles.gentoo.org/snapshots/gentoo-latest.tar.xz"
 
-echo ">>> 1. Wiping disk..."
+echo ">>> 1. Wiping and partitioning..."
 swapoff -a || true
-# Create MBR, 4GB Swap, Root, and set Boot flag on sda2
 printf "o\nn\np\n1\n\n+4G\nt\n82\nn\np\n2\n\n\na\n2\nw\n" | fdisk $DISK
 
 echo ">>> 2. Formatting..."
-mkswap $PART_SWAP
-swapon $PART_SWAP
+mkswap $PART_SWAP && swapon $PART_SWAP
 mkfs.ext4 -F $PART_ROOT
 mount $PART_ROOT /mnt/gentoo
 
-echo ">>> 3. Downloading your Stage3..."
+echo ">>> 3. Downloading Stage3..."
 cd /mnt/gentoo
 wget "$STAGE3_URL" -O stage3.tar.xz
-
-echo ">>> 4. Unpacking..."
 tar xpvf stage3.tar.xz --xattrs-include='*.*' --numeric-owner
 rm stage3.tar.xz
 
-echo ">>> 5. Preparing chroot..."
+echo ">>> 4. Manual Repo Setup (Fixing 'Invalid Location')..."
+mkdir -p /mnt/gentoo/var/db/repos/gentoo
+cd /mnt/gentoo/var/db/repos/gentoo
+wget "$SNAP_URL" -O gentoo.tar.xz
+echo ">>> Unpacking repo (Celeron is sweating...)"
+tar xpvf gentoo.tar.xz --strip-components=1
+rm gentoo.tar.xz
+
+echo ">>> 5. Preparing environment..."
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
@@ -36,42 +42,38 @@ mount --make-rslave /mnt/gentoo/dev
 mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
-echo ">>> 6. Inside CHROOT: Starting the engine..."
-cat <<EOF > /mnt/gentoo/install_final.sh
+echo ">>> 6. Entering CHROOT for the final fight..."
+cat <<EOF > /mnt/gentoo/ultimate_fix.sh
 #!/bin/bash
 source /etc/profile
 
-# Fixing repository config
-mkdir -p /etc/portage/repos.conf
-cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
+# Force link profile (Hardcore mode)
+ln -snf /var/db/repos/gentoo/profiles/default/linux/amd64/23.0/desktop /etc/portage/make.profile
 
-# Portage tuning
+# Portage config
+mkdir -p /etc/portage/repos.conf
+echo -e "[gentoo]\nlocation = /var/db/repos/gentoo" > /etc/portage/repos.conf/gentoo.conf
 echo 'ACCEPT_LICENSE="*"' >> /etc/portage/make.conf
 echo 'MAKEOPTS="-j2"' >> /etc/portage/make.conf
 mkdir -p /etc/portage/package.accept_keywords
 echo "sys-kernel/gentoo-kernel-bin ~amd64" > /etc/portage/package.accept_keywords/kernel
 
-# Syncing repo
-emerge-webrsync
+# Installing GRUB and Kernel with ONE J-THREASHOLD
+echo ">>> Installing GRUB and Kernel (This is it...)"
+CONFIG_PROTECT="-*" emerge --ask=n --oneshot sys-boot/grub sys-kernel/gentoo-kernel-bin net-misc/dhcpcd
 
-# Setting profile explicitly to avoid 'invalid' errors
-eselect profile set default/linux/amd64/23.0/desktop
-
-# Installation (Kernel, Grub, Network)
-CONFIG_PROTECT="-*" emerge --ask=n sys-boot/grub sys-kernel/gentoo-kernel-bin net-misc/dhcpcd
-
-# Bootloader setup
+# Bootloader
 grub-install --target=i386-pc $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# System final touches
+# Final touches
 echo "$PART_ROOT / ext4 noatime 0 1" > /etc/fstab
 echo "$PART_SWAP none swap sw 0 0" >> /etc/fstab
 rc-update add dhcpcd default
 echo "root:gentoo" | chpasswd
 
-echo ">>> EVERYTHING FINISHED SUCCESSFULLY!"
+echo ">>> HOLY SHIT, IT'S FINISHED!"
 EOF
 
-chmod +x /mnt/gentoo/install_final.sh
-chroot /mnt/gentoo /install_final.sh
+chmod +x /mnt/gentoo/ultimate_fix.sh
+chroot /mnt/gentoo /ultimate_fix.sh
