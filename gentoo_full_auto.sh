9b78500eef9168b5f0fe8a1c31eab168d7d8ffb7
Timex <timexdev123@gmail.com>
2026-02-22 12:08:54 +0700

    Update Gentoo installation script with improved steps

--- gentoo_full_auto.sh
+++ gentoo_full_auto.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 set -e
 
-# --- ПЕРЕМЕННЫЕ ---
 DISK="/dev/sda"
 PART_SWAP="${DISK}1"
 PART_ROOT="${DISK}2"
-# Ссылка на самый свежий Stage3 (OpenRC Desktop)
+# Stage3 URL - make sure you have internet!
 STAGE3_URL="https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-desktop-openrc-20260215T164556Z.tar.xz"
 
-echo ">>> 1. Подготовка диска..."
+echo ">>> 1. Wiping disk and creating partitions..."
 swapoff -a || true
-# Создаем таблицу разделов (DOS), 4GB Swap, остальное Root
-printf "o\nn\np\n1\n\n+4G\nt\n82\nn\np\n2\n\n\nw\n" | fdisk $DISK
+# Wipe partition table and create new MBR (DOS)
+# 4GB Swap, rest for Root, Set Root partition as Bootable (*)
+printf "o\nn\np\n1\n\n+4G\nt\n82\nn\np\n2\n\n\na\n2\nw\n" | fdisk $DISK
 
+echo ">>> 2. Formatting partitions..."
 mkswap $PART_SWAP
 swapon $PART_SWAP
 mkfs.ext4 -F $PART_ROOT
 mount $PART_ROOT /mnt/gentoo
 
-echo ">>> 2. Загрузка и распаковка системы..."
+echo ">>> 3. Downloading and unpacking Stage3..."
 cd /mnt/gentoo
 wget $STAGE3_URL
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 rm stage3-*.tar.xz
+sync
 
-echo ">>> 3. Монтирование окружения..."
+echo ">>> 4. Mounting system directories..."
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
@@ -34,40 +36,41 @@ mount --make-rslave /mnt/gentoo/dev
 mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
-echo ">>> 4. Вход в CHROOT и автоматическая магия..."
-cat <<EOF > /mnt/gentoo/final.sh
+echo ">>> 5. Entering CHROOT for installation..."
+cat <<EOF > /mnt/gentoo/finalize.sh
 #!/bin/bash
 source /etc/profile
 
-# Настройка fstab
+# Portage Config for Celeron
+echo 'ACCEPT_LICENSE="*"' >> /etc/portage/make.conf
+echo 'MAKEOPTS="-j2"' >> /etc/portage/make.conf
+echo 'PORTAGE_NICENESS=19' >> /etc/portage/make.conf
+mkdir -p /etc/portage/package.accept_keywords
+echo "sys-kernel/gentoo-kernel-bin ~amd64" > /etc/portage/package.accept_keywords/kernel
+
+# Fstab setup
 echo "$PART_ROOT / ext4 noatime 0 1" > /etc/fstab
 echo "$PART_SWAP none swap sw 0 0" >> /etc/fstab
 
-# Настройка Portage (чтобы не было варнов и запросов)
-mkdir -p /etc/portage/package.accept_keywords
-echo "sys-kernel/gentoo-kernel-bin ~amd64" > /etc/portage/package.accept_keywords/kernel
-echo "ACCEPT_LICENSE=\"*\"" >> /etc/portage/make.conf
-echo "MAKEOPTS=\"-j2\"" >> /etc/portage/make.conf
-
-# Синхронизация
+# Sync and install Kernel + GRUB
+echo ">>> Syncing portage (emerge-webrsync)..."
 emerge-webrsync
-
-# Автоматическое принятие всех изменений конфигов (etc-update)
+echo ">>> Installing kernel and bootloader (this will take a LONG time)..."
 CONFIG_PROTECT="-*" emerge --ask=n sys-kernel/gentoo-kernel-bin sys-boot/grub net-misc/dhcpcd
 
-# Установка GRUB
-grub-install $DISK
+# Final Bootloader configuration
+echo ">>> Installing GRUB to MBR..."
+grub-install --target=i386-pc $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Настройка сети и пароля
+# Network and root password
 rc-update add dhcpcd default
 echo "root:gentoo" | chpasswd
 
-echo "--------------------------------------------------"
-echo "ВСЁ! Система готова. Пароль: gentoo"
-echo "Теперь пиши: exit, umount -R /mnt/gentoo, reboot"
-echo "--------------------------------------------------"
+echo ">>> FINISHED! System is ready."
+echo ">>> Password is: gentoo"
+echo ">>> Type: exit, then umount -R /mnt/gentoo, then reboot."
 EOF
 
-chmod +x /mnt/gentoo/final.sh
-chroot /mnt/gentoo /final.sh
+chmod +x /mnt/gentoo/finalize.sh
+chroot /mnt/gentoo /finalize.sh
